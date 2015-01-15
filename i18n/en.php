060d85c957bdb8df77b5070858945e9cc81951a8
Butch Stine <bullett@ex.ua>
2015-01-15 04:13:23 +0200

    English translation file fixed

--- i18n/en.php
+++ i18n/en.php
@@ -1,24 +1,8 @@
 <?php defined('SYSPATH') OR die('No direct script access.');
 return array(
-    '' => '',
-    'item' => 'новость',
-    'Site News' => 'Новости',
+    'Classified ads' => 'Classified ads',
+    'Add ad' => 'Add ad',
+    'Select category' => 'Select category',
 
-    'Title' => 'Title',
-    'Category' => 'Category',
-    'Description' => 'Description',
-    'Text' => 'Text',
-    'Visible' => 'Visible',
-    'Draft' => 'Draft',
-    'Photos' => 'Photos',
-
-    'News Categories' => 'News Categories',
-    'category' => 'category',
-    'Part' => 'Part',
-    'Name' => 'Name',
-
-    'NewId' => 'News',
-    'Width' => 'Width',
-    'Height' => 'Height',
-    'Extension' => 'Extension',
+//    '' => '',
 );

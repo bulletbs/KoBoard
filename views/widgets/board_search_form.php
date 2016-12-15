65df51ba1414e83a2af0c53a2e05f7c1bb711586
Butch Stine <bulletua@gmail.com>
2016-12-15 17:14:07 +0200

    - исправления шаблона формы поиска

--- views/widgets/board_search_form.php
+++ views/widgets/board_search_form.php
@@ -1,9 +1,8 @@
 <?php defined('SYSPATH') or die('No direct script access.');?>
-<!--noindex-->
 <div class="board_search_form">
 <?php echo Form::open($form_action, array('id'=>'boardTopForm', 'method'=>'get'))?>
     <div id="queryInput" class="selector">
-        <?php echo Form::input('query', Arr::get($_GET, 'query'), array('placeholder'=>'Найти по тексту', 'class'=>'query', 'id'=>'serchformQuery'))?>
+        <?php echo Form::input('query', Arr::get($_GET, 'query'), array('placeholder'=>'Поиск по объявлениям', 'class'=>'query', 'id'=>'serchformQuery'))?>
     </div>
     <div id="regionLabel" class="selector">
         <?php echo Form::input(NULL, $region_name, array('placeholder'=>'Область', 'readonly'=>'readonly', 'id'=>'regionTopInput'))?>
@@ -24,4 +23,3 @@
     <div class="clear"></div>
 <?php echo Form::close()?>
 </div>
-<!--/noindex-->

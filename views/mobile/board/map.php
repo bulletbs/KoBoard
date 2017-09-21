78c71609a707ae9bf678cc1df9498343c8c632c8
Butch <bulletua@gmail.com>
2017-09-21 17:57:26 +0300

    шаблон главной страницы разделен на карту и последние объявления последние объявления отображаются при установке опции show_ads_on_main в конфиге модуля (board)

--- views/mobile/board/map.php
+++ views/mobile/board/map.php
@@ -1,15 +1,1 @@
 <?php defined('SYSPATH') OR die('No direct script access.');?>
-<!--<ul class="uk-grid uk-grid-width-1-2">-->
-<ul class="uk-list uk-list-striped">
-    <li><a href="/all/biznes-i-uslugi.html" title="Бизнес и услуги">Бизнес и услуги</a></li>
-    <li><a href="/all/dom-i-sad.html" title="Дом и сад">Дом и сад</a></li>
-    <li><a href="/all/ebay.html" title="Куплю">Куплю</a></li>
-    <li><a href="/all/moda-i-stil.html" title="Мода и стиль">Мода и стиль</a></li>
-    <li><a href="/all/rabota.html" title="Работа">Работа</a></li>
-    <li><a href="/all/khobbi-otdykh-i-sport.html" title="Хобби, отдых и спорт">Хобби, отдых и спорт</a></li>
-    <li><a href="/all/detskij-mir.html" title="Детский мир">Детский мир</a></li>
-    <li><a href="/all/zhivotnye.html" title="Животные">Животные</a></li>
-    <li><a href="/all/nedvizhimost.html" title="Недвижимость">Недвижимость</a></li>
-    <li><a href="/all/transport.html" title="Транспорт">Транспорт</a></li>
-    <li><a href="/all/ehlektronika.html" title="Электроника">Электроника</a></li>
-</ul>
